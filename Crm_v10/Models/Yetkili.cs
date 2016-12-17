02d302ae9a99f12889f85a87948cc82cddb09920
sematufan <sema_tufan@hotmail.com>
2016-12-17 18:46:23 +0300

    Sema Tufan 17.12.2016 saat 18.45
    Yetkili Sayfası Uyarı Mesajları Düzeltildi.
    Yetkili Kartları Create ve Editte ki Label uzunluğu düzeltildi.

--- Crm_v10/Models/Yetkili.cs
+++ Crm_v10/Models/Yetkili.cs
@@ -11,29 +11,33 @@ namespace Crm_v10.Models
     public partial class Yetkili
     {
         public int ID { get; set; }
-        [Required]
+        //Bu alanı zorunlu hale getiriyoruz. Böylelikle boş geçilemeyecek.
+        [Required(ErrorMessage = "Lütfen Yetkili Kodu Giriniz.")]
         [StringLength(20)]
         public string YetkiliKodu { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Lütfen Yetkili  Adı Giriniz.")]
         [StringLength(50)]
         public string YetkiliAd { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Lütfen Yetkili  Soyadı Giriniz.")]
         [StringLength(50)]
         public string YetkiliSoyad { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Lütfen GSM1 Giriniz.")]
         [StringLength(11)]
         public string YetkiliGSM1 { get; set; }
        
         [StringLength(11)]
         public string YetkiliGSM2 { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Lütfen Yetkili  Mail1 Giriniz.")]
         [StringLength(50)]
         public string YetkiliMail1 { get; set; }
         
         [StringLength(50)]
         public string YetkiliMail2 { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Yetkili Doğum Tarihi Giriniz.")]
         public DateTime YetkiliDogumTarihi { get; set; }
     }
 }
